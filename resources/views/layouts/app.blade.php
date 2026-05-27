2855325fe59f4047d6b17310c8504bf81916453d
SANYA123VERMA <sanyaverma444@gmail.com>
2026-05-27 12:49:22 +0530

    Fix missing CSS link in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <title>@yield('title', 'LaunchPad Market')</title>
 <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,600;0,700;1,600;1,700&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
